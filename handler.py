69b3edf8ea65675b1f656489f9391b6bcb428c2b
EJ Ozyazgan <ejozyazgan@gmail.com>
2024-05-04 20:59:55 -0700

    driver options

--- handler.py
+++ handler.py
@@ -3,6 +3,7 @@ import logging
 import pandas as pd
 import boto3
 import json
+from tempfile import mkdtemp
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -67,17 +68,26 @@ def getTickerSymbols(event, context):
 def getSiteData(url, ticker):
 
 	options = webdriver.ChromeOptions()
-	options.binary_location = '/opt/headless-chromium'
-	options.add_argument('--headless')
-	options.add_argument('--no-sandbox')
-	options.add_argument('--single-process')
-	options.add_argument('--disable-dev-shm-usage')
+	service = webdriver.ChromeService("/opt/chromedriver")
 
-	driver = webdriver.Chrome(options=options)
-		
+	options.binary_location = '/opt/chrome/chrome'
+	options.add_argument("--headless=new")
+	options.add_argument('--no-sandbox')
+	options.add_argument("--disable-gpu")
+	options.add_argument("--window-size=1280x1696")
+	options.add_argument("--single-process")
+	options.add_argument("--disable-dev-shm-usage")
+	options.add_argument("--disable-dev-tools")
+	options.add_argument("--no-zygote")
+	options.add_argument(f"--user-data-dir={mkdtemp()}")
+	options.add_argument(f"--data-path={mkdtemp()}")
+	options.add_argument(f"--disk-cache-dir={mkdtemp()}")
+	options.add_argument("--remote-debugging-port=9222")
+
+	driver = webdriver.Chrome(options=options, service=service)
+	
 	try:  
 		print(url)
-
 		
 		driver.get(url)
 
