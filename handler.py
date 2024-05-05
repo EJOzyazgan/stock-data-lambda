c90e5c64cc931a39ea3efa1bfaa764b5cebe5a5b
EJ Ozyazgan <ejozyazgan@gmail.com>
2024-05-04 21:27:02 -0700

    add EC import

--- handler.py
+++ handler.py
@@ -7,6 +7,7 @@ from tempfile import mkdtemp
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
