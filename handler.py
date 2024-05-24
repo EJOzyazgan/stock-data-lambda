edf0e29f10661cbd73387b650269886f950f1a39
EJ Ozyazgan <ejozyazgan@gmail.com>
2024-05-23 17:23:07 -0700

    save message screen shopt

--- handler.py
+++ handler.py
@@ -188,18 +188,18 @@ def sendMessage(message):
 
 		logger.info('Message Page')
 
-		# driver.save_screenshot('/tmp/screenshot.png')
+		driver.save_screenshot('/tmp/screenshot.png')
 
-		# logger.info('Screenshot Created')
+		logger.info('Screenshot Created')
 
-		# lst = os.listdir("/tmp")
-		# logger.info(lst)
+		lst = os.listdir("/tmp")
+		logger.info(lst)
 
-		# s3_client = boto3.client('s3')
+		s3_client = boto3.client('s3')
 
-		# s3_client.upload_file('/tmp/screenshot.png', 'stock-data-debug-bucket', 'screenshot.png')
+		s3_client.upload_file('/tmp/screenshot.png', 'stock-data-debug-bucket', 'screenshot.png')
 
-		# logger.info('Screenshot Saved')
+		logger.info('Screenshot Saved')
 
 		sendButton = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.XPATH, '//*[@id="recipientArea"]/div[3]/button')))
 		ActionChains(driver).scroll_to_element(sendButton).perform()
